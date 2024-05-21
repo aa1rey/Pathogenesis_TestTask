5164824356a8e2a1b19fe39105a28a93644e5999
aa1rey <artemtoples@yandex.ru>
2024-05-21 14:44:04 +0300

    init commit

---
+++ Source/PathogenesisTestTask/private/PathogenesisTestTask.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PathogenesisTestTask.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PathogenesisTestTask, "PathogenesisTestTask" );
+ 
---
+++ Source/PathogenesisTestTask/public/PathogenesisTestTaskGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PathogenesisTestTaskGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APathogenesisTestTaskGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APathogenesisTestTaskGameMode();
+};
+
+
+
